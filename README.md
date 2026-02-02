b1be742938eb66ab921cc64f15bd4b6dff808bcb
h4sh5 <38898566+h4sh5@users.noreply.github.com>
2026-02-02 10:44:13 +1000

    custom rules support

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
-# WAFfles - a WAF template
+# WAFfles - a small simple WAF
 
-A golang HTTP proxy that forwards requests to an origin URL after checking a blocking function.
+A golang HTTP WAF that forwards requests to an origin URL after checking blocking rules.
+
+It can act as a reverse proxy that functions as the web application's access point, blocking malicious requests.
+
+Listens on port 80 by default.
 
 ## usage
 
+### Compiling
+
 `go build`
 
-`./waffles <url of origin>`
+### Running
 
-example (if origin URL is at `http://localhost:8080`)
+`./waffles <url of origin> <rules file>`
 
-`./waffles http://localhost:8080`
+example (if origin URL is at `http://localhost:8080` and you are using the example rules)
+
+`./waffles http://localhost:8080 example.rules.json`
+
+### Rule writing
+
+An example JSON rules file is provided in the repo.
+
+Each rule has multiple items that combine using an OR / AND condition. They also 
+
+All rules are processed, and if any rules match the request will be blocked, returning HTTP status 406.
+
+Example rule that blockes .. in path, query and body, and blocks HTML tags in query and `<script` in body (not taking in consideration HTTP encoding):
+
+```json
+[
+	{
+		"items": [
+			{"id":"no-dot-dot-path", "part":"path", "regex":"\\.\\."},
+			{"id":"no-dot-dot-query", "part":"query", "regex":"\\.\\."},
+			{"id":"no-dot-dot-body", "part":"body", "regex":"\\.\\."}
+		],
+		"condition": "OR"
+	},
+	{
+		"items": [
+			{"id":"no-html-tag-query", "part":"query", "regex":"<"},
+			{"id":"no-html-script-tag-body", "part":"body", "regex":"<script"}
+		],
+		"condition": "OR"
+	}
+]
+```
+
 
--- main.go
+++ main.go
@@ -7,36 +7,100 @@ import (
 	"io/ioutil"
 	"fmt"
 	"io"
+	"bufio"
 	"net/http"
 	"log"
-	"strings"
+	// "strings"
 	"os"
+	"regexp"
+	"encoding/json"
 )
 
+type RuleItem struct {
+	Id string `json:"id"`
+	Regex string `json:"regex"` // the actual rule content
+	Part string `json:"part"` // path | query | body
+}
+
+type Rule struct {
+	RuleItems []RuleItem `json:"items"`
+	Condition string `json:"condition"` // AND | OR 
+}
+
 var forwardUrl string
+var GlobalRulesData []Rule
+var rulesFile string
 
 // implement more blocking functionality here
-func block_request(r *http.Request, reqBody []byte) bool {
+// return true for blocked, false for allowed
+func block_request(r *http.Request, reqBody []byte, rules []Rule) bool {
 	// return true or false based on blocking
+	// reqBodyStr := string(reqBody)
+	for i := 0; i < len(rules); i++ {
+		rule := rules[i]
+		ruleItems := rule.RuleItems
+		reqBodyString := string(reqBody)
+		matchedRulesCount := 0
 
-	if strings.Contains(r.URL.Path, "EICAR") { // EICAR test string
-		return true; // block this request
+		for j := 0; j < len(ruleItems); j++ {
+
+			if (ruleItems[j].Part == "path") {
+				
+				matched, _ := regexp.MatchString(ruleItems[j].Regex, r.URL.Path)
+				if matched  {
+					log.Printf("MATCH regex: %v, url path: %s\n", ruleItems[j].Regex, r.URL.Path)
+					matchedRulesCount ++;
+					if rule.Condition == "OR" {
+						return true
+					}
+				}
+			}
+			if (ruleItems[j].Part == "query") {
+				
+				matched, _ := regexp.MatchString(ruleItems[j].Regex, r.URL.RawQuery)
+				if matched {
+					log.Printf("MATCH regex: %v, query: %s\n", ruleItems[j].Regex, r.URL.RawQuery)
+					matchedRulesCount ++;
+					if rule.Condition == "OR" {
+						return true
+					}
+				}
+			}
+			if (ruleItems[j].Part == "body") {
+				
+				// log.Printf("regex: %v, body: %s\n", ruleItems[j].Regex, )
+				matched, _ := regexp.MatchString(ruleItems[j].Regex, reqBodyString)
+				if matched {
+					log.Printf("MATCH regex: %v, query: %s\n", ruleItems[j].Regex, r.URL.RawQuery)
+					matchedRulesCount ++;
+					if rule.Condition == "OR" {
+						return true
+					}
+				}
+			}
+
+		}
+		if rule.Condition == "AND" && matchedRulesCount == len(ruleItems) {
+			log.Printf("BLOCK matchedRulesCount: %v, len(ruleItems): %v\n", matchedRulesCount, len(ruleItems))
+			return true
+		}
 	}
 
 	return false;
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
+	log.Printf("got / request\n")
+	loadRules(rulesFile)
 
 	reqBody, err := ioutil.ReadAll(r.Body)
-	fmt.Printf("reqBody:\n%s", reqBody)
+	log.Printf("reqBody:\n%s", reqBody)
 
 	if err != nil {
-		fmt.Printf("block_request: could not read response body: %s\n", err)	
+		log.Printf("block_request: could not read response body: %s\n", err)	
 	}
 
-	if (block_request(r, reqBody)) {
+	if (block_request(r, reqBody, GlobalRulesData)) {
 		w.WriteHeader(http.StatusNotAcceptable) // block with 406
 		io.WriteString(w, "Blocked by WAF!\n")
 		return
@@ -45,7 +109,7 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 		requestUrl := fmt.Sprintf("%s%s?%s", forwardUrl, r.URL.Path, r.URL.RawQuery)
 		req, err := http.NewRequest(r.Method, requestUrl, bytes.NewBuffer(reqBody))
 		if err != nil {
-			fmt.Printf("client: could not create request: %s\n", err)
+			log.Printf("client: could not create request: %s\n", err)
 		}
 
 		// pass thru request headers
@@ -55,12 +119,12 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 
 		res, err := http.DefaultClient.Do(req)
 		if err != nil {
-			fmt.Printf("client: error making http request: %s\n", err)
+			log.Printf("client: error making http request: %s\n", err)
 		}
 
 		resBody, err := ioutil.ReadAll(res.Body)
 		if err != nil {
-			fmt.Printf("client: could not read response body: %s\n", err)
+			log.Printf("client: could not read response body: %s\n", err)
 		}
 
 		// pass thru response headers as well
@@ -72,17 +136,48 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 		w.Write(resBody)
 
 	}
+}
+
+func loadRules(filename string) {
+	rulesFile, err := os.Open(filename)
+	if err != nil {
+	    log.Println("Error opening rules file.")
+		return
+	}
+	reader := bufio.NewReader(rulesFile)
+	bytes, err := io.ReadAll(reader)
+	rulesFile.Close()
+	if (err != nil) {
+		log.Println("Error reading rules file.")
+		return
+	}
 	
-	
+	err = json.Unmarshal(bytes, &GlobalRulesData)
+
+	if err != nil {
+		log.Println("Error unmarshalling rules file.")
+		return
+	}
+	log.Printf("Rules loaded: %#v",GlobalRulesData)
 }
 
 func main() {
 	forwardUrl = os.Args[1]
+	if len(os.Args) > 2 {
+		rulesFile = os.Args[2]
+	} else{
+		rulesFile = "rules.json" // default path
+	}
+	log.Printf("using rules file: %s\n", rulesFile)
+	
+	// load rules
 	http.HandleFunc("/", getRoot)
-	err := http.ListenAndServe(":80", nil)
+	// http.HandleFunc("*", getRoot)
+	listenAddr := ":80"
+	log.Printf("Listening on %v\n", listenAddr)
+	err := http.ListenAndServe(listenAddr, nil)
 	if (err != nil) {
 		log.Fatalf("Err starting http server: %s", err)
 	}
 }
-
 
